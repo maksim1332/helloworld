541f1531afca55f4583703b93cf8730ae0ff6f9b
USER <mas9n9-mas9n91332@yandex.ru>
2024-08-27 23:20:03 +0500

    initial commit).

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
-        //Sistem.iut.println("Hello, World!");
+        //Sistem.out.println("Hello, World!");
     }
 }
